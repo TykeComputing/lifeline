6158bcc102f0748c454beab1dcfd40b540898a6d
Peter Clark <pclark@peterclark.net>
2014-05-25 18:11:02 -0700

    (NC) Getting CMake script working on windows.
    - Working on finding libraries. First pass will be messy.

--- CMake/Modules/FindSDL2.cmake
+++ CMake/Modules/FindSDL2.cmake
@@ -53,59 +53,152 @@
 # reasons because not all systems place things in SDL2/ (see FreeBSD).
 
 #=============================================================================
-# Copyright 2003-2009 Kitware, Inc.
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
+# CMake - Cross Platform Makefile Generator
+# Copyright 2000-2014 Kitware, Inc.
+# Copyright 2000-2011 Insight Software Consortium
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# 
+# * Neither the names of Kitware, Inc., the Insight Software Consortium,
+#   nor the names of their contributors may be used to endorse or promote
+#   products derived from this software without specific prior written
+#   permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# ------------------------------------------------------------------------------
+# 
+# The above copyright and license notice applies to distributions of
+# CMake in source and binary form.  Some source files contain additional
+# notices of original copyright by their contributors; see each source
+# for details.  Third-party software packages supplied with CMake under
+# compatible licenses provide their own copyright notices documented in
+# corresponding subdirectories.
+# 
+# ------------------------------------------------------------------------------
+# 
+# CMake was initially developed by Kitware with the following sponsorship:
+# 
+#  * National Library of Medicine at the National Institutes of Health
+#    as part of the Insight Segmentation and Registration Toolkit (ITK).
+# 
+#  * US National Labs (Los Alamos, Livermore, Sandia) ASC Parallel
+#    Visualization Initiative.
+# 
+#  * National Alliance for Medical Image Computing (NAMIC) is funded by the
+#    National Institutes of Health through the NIH Roadmap for Medical Research,
+#    Grant U54 EB005149.
+# 
+#  * Kitware, Inc.
 #=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
 
-SET(SDL2_SEARCH_PATHS
-	~/Library/Frameworks
-	/Library/Frameworks
-	/usr/local
-	/usr
-	/sw # Fink
-	/opt/local # DarwinPorts
-	/opt/csw # Blastwave
-	/opt
-)
+# Lifeline Engine specific modifications:
+#   - Changed SDL2_SEARCH_PATHS values to be OS dependent (since they are).
+#     - Added Lifeline Engine specific path to external libraries.
+# Lifeline Engine specific TODO:
+#   - Clean up code added. (5/25/2014)
 
-FIND_PATH(SDL2_INCLUDE_DIR SDL.h
-	HINTS
-	$ENV{SDL2DIR}
-	PATH_SUFFIXES include/SDL2 include
-	PATHS ${SDL2_SEARCH_PATHS}
-)
+################################
+#
+# WINDOWS
+#
+################################
+IF("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
+	FIND_PATH(SDL2_INCLUDE_DIR SDL.h
+		HINTS $ENV{SDL2DIR}
+		PATHS external/windows/include/SDL2
+	)
 
-FIND_LIBRARY(SDL2_LIBRARY_TEMP
-	NAMES SDL2
-	HINTS
-	$ENV{SDL2DIR}
-	PATH_SUFFIXES lib64 lib
-	PATHS ${SDL2_SEARCH_PATHS}
-)
+	FIND_LIBRARY(SDL2_LIBRARY_TEMP
+		NAMES SDL2
+		HINTS $ENV{SDL2DIR}
+		PATH_SUFFIXES lib64 lib
+		PATHS external/windows/lib/x86
+	)
 
-IF(NOT SDL2_BUILDING_LIBRARY)
-	IF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
-		# Non-OS X framework versions expect you to also dynamically link to
-		# SDL2main. This is mainly for Windows and OS X. Other (Unix) platforms
-		# seem to provide SDL2main for compatibility even though they don't
-		# necessarily need it.
-		FIND_LIBRARY(SDL2MAIN_LIBRARY
-			NAMES SDL2main
-			HINTS
-			$ENV{SDL2DIR}
-			PATH_SUFFIXES lib64 lib
-			PATHS ${SDL2_SEARCH_PATHS}
-		)
-	ENDIF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
-ENDIF(NOT SDL2_BUILDING_LIBRARY)
+	IF(NOT SDL2_BUILDING_LIBRARY)
+		IF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
+			# Non-OS X framework versions expect you to also dynamically link to
+			# SDL2main. This is mainly for Windows and OS X. Other (Unix) platforms
+			# seem to provide SDL2main for compatibility even though they don't
+			# necessarily need it.
+			FIND_LIBRARY(SDL2MAIN_LIBRARY
+				NAMES SDL2main
+				HINTS
+				$ENV{SDL2DIR}
+				PATH_SUFFIXES lib64 lib
+				PATHS ${SDL2_SEARCH_PATHS}
+			)
+		ENDIF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
+	ENDIF(NOT SDL2_BUILDING_LIBRARY)
+
+################################
+#
+# LINUX
+#
+################################
+ELSEIF("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
+	SET(SDL2_SEARCH_PATHS 
+		~/Library/Frameworks
+		/Library/Frameworks
+		/usr/local
+		/usr
+		/sw # Fink
+		/opt/local # DarwinPorts
+		/opt/csw # Blastwave
+		/opt
+	)
+
+	FIND_PATH(SDL2_INCLUDE_DIR SDL.h
+		HINTS $ENV{SDL2DIR}
+		PATH_SUFFIXES include/SDL2 include
+		PATHS ${SDL2_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(SDL2_LIBRARY_TEMP
+		NAMES SDL2
+		HINTS $ENV{SDL2DIR}
+		PATH_SUFFIXES lib64 lib
+		PATHS ${SDL2_SEARCH_PATHS}
+	)
+
+	IF(NOT SDL2_BUILDING_LIBRARY)
+		IF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
+			# Non-OS X framework versions expect you to also dynamically link to
+			# SDL2main. This is mainly for Windows and OS X. Other (Unix) platforms
+			# seem to provide SDL2main for compatibility even though they don't
+			# necessarily need it.
+			FIND_LIBRARY(SDL2MAIN_LIBRARY
+				NAMES SDL2main
+				HINTS
+				$ENV{SDL2DIR}
+				PATH_SUFFIXES lib64 lib
+				PATHS ${SDL2_SEARCH_PATHS}
+			)
+		ENDIF(NOT ${SDL2_INCLUDE_DIR} MATCHES ".framework")
+	ENDIF(NOT SDL2_BUILDING_LIBRARY)
+
+ENDIF()
 
 # SDL2 may require threads on your system.
 # The Apple build may not need an explicit flag because one of the
