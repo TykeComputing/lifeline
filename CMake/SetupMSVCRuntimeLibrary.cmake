72995dfabad1ed3bcffc19918c1e9937af22b8eb
Peter Clark <pclark@peterclark.net>
2014-06-02 03:23:23 -0700

    CMAKE_CXX_FLAGS_<CONFIG> do not appear to be setting properly. Researching.

--- CMake/SetupMSVCRuntimeLibrary.cmake
+++ CMake/SetupMSVCRuntimeLibrary.cmake
@@ -20,31 +20,46 @@
 # Original implementation: 
 # http://stackoverflow.com/questions/10113017/setting-the-msvc-runtime-in-cmake
 
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+
 set(MSVC_RUNTIME "dynamic" CACHE STRING
   "Valid values: dynamic and static. Controls whether /MD* or /MT* are used")
 
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  # Store all variables of CMAKE_C**_FLAGS_* in CONFIG_FLAG_VARS
+  foreach(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+    set(UPPER_VAR_TEMP)
+    string(TOUPPER "CMAKE_C_FLAGS_${CONFIG_TYPE}" UPPER_VAR_TEMP)
+    set(CONFIG_FLAG_VARS ${CONFIG_FLAG_VARS} ${UPPER_VAR_TEMP})
+    string(TOUPPER "CMAKE_CXX_FLAGS_${CONFIG_TYPE}" UPPER_VAR_TEMP)
+    set(CONFIG_FLAG_VARS ${CONFIG_FLAG_VARS} ${UPPER_VAR_TEMP})
+    unset(UPPER_VAR_TEMP)
+  endforeach()  
 
-  if(${MSVC_RUNTIME} STREQUAL "dynamic")    
-    foreach(config_type ${CMAKE_CONFIGURATION_TYPES})
-      if(${config_type} MATCHES "/MT")
-        string(REGEX REPLACE "/MT" "/MD" ${config_type} "${${config_type}}")
-      endif()
-    endforeach()
-
-  elseif(${MSVC_RUNTIME} STREQUAL "static")
-    foreach(config_type ${CMAKE_CONFIGURATION_TYPES})
-      if(${config_type} MATCHES "/MD")
-        string(REGEX REPLACE "/MD" "/MT" ${config_type} "${${config_type}}")
-      endif()
-    endforeach()
-
-  else()
-    message(FATAL_ERROR
-      "Invalid value given for MSVC_RUNTIME: ${MSVC_RUNTIME}, valid values are static and dynamic.")
-    
-  endif()
-  
   message(STATUS "MSVC: Forcing use of ${MSVC_RUNTIME}ally-linked runtime.")
 
+  # For each variable in CONFIG_FLAG_VARS, replace all instances of MSVC runtime specifying flags
+  #   with the appropriate flag.
+  if(${MSVC_RUNTIME} STREQUAL "dynamic")    
+    foreach(CONFIG_FLAG_VAR_IT ${CONFIG_FLAG_VARS})
+      if(${CONFIG_FLAG_VAR_IT} MATCHES "/MT")
+        string(REGEX REPLACE "/MT" "/MD" ${CONFIG_FLAG_VAR_IT} "${${CONFIG_FLAG_VAR_IT}}")
+      endif()
+    endforeach()
+  elseif(${MSVC_RUNTIME} STREQUAL "static")
+    foreach(CONFIG_FLAG_VAR_IT ${CONFIG_FLAG_VARS})
+      if(${CONFIG_FLAG_VAR_IT} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${CONFIG_FLAG_VAR_IT} "${${CONFIG_FLAG_VAR_IT}}")
+      endif()
+    endforeach()
+  else()
+    message(FATAL_ERROR
+      "Invalid value given for MSVC_RUNTIME: ${MSVC_RUNTIME}, valid values are static and dynamic.")    
+  endif()
+
+  # message(STATUS "Initial build flags:")
+  # foreach(CONFIG_FLAG_VAR_IT ${CONFIG_FLAG_VARS})
+  #   message(STATUS "  '${CONFIG_FLAG_VAR_IT}': ${${CONFIG_FLAG_VAR_IT}}")
+  # endforeach()
+  # message(STATUS "")
+  
 endif()
