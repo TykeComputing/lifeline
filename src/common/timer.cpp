818afbb66b75fee8b3aa43110a675f8be65a3dc6
Peter Clark <pclark@peterclark.net>
2014-06-06 18:25:31 -0700

    Added timer.

---
+++ src/common/timer.cpp
@@ -0,0 +1,51 @@
+/*
+************************************************************************************************
+Copyright 2014 Peter Clark
+
+This file is part of Lifeline Engine.
+
+Lifeline Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lifeline Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lifeline Engine.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************************************
+*/
+
+#include "timer.h"
+
+#include <common/error.h>
+
+namespace LE
+{
+
+
+timer::timer(void)
+{
+  reset();
+}
+
+void timer::reset(void)
+{
+  p_perf_freq = SDL_GetPerformanceFrequency();
+  p_perf_count_start = SDL_GetPerformanceCounter();
+}
+
+float timer::poll(void) const
+{
+  Uint64 perf_count_end = SDL_GetPerformanceCounter();
+  Uint64 perf_count_elapsed = perf_count_end - p_perf_count_start;
+  
+  LE_ERRORIF(p_perf_freq != SDL_GetPerformanceFrequency(),
+    "Timer error, frequency when polled is different from frequency when reset!");
+  return static_cast<float>(perf_count_elapsed) / p_perf_freq;
+}
+
+} // namespace LE
---
+++ src/common/timer.h
@@ -0,0 +1,46 @@
+/*
+************************************************************************************************
+Copyright 2014 Peter Clark
+
+This file is part of Lifeline Engine.
+
+Lifeline Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lifeline Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lifeline Engine.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************************************
+*/
+
+#ifndef LE_COMMON_TIMER_H
+#define LE_COMMON_TIMER_H
+
+#include <SDL2/SDL_timer.h>
+
+namespace LE
+{
+
+// SDL_Init must be called before constructing a timer
+class timer
+{
+public:
+  timer(void);
+
+  void reset(void);
+  float poll(void) const;
+
+private:
+  Uint64 p_perf_count_start = 0u;
+  Uint64 p_perf_freq = 0u;
+};
+
+} // namespace LE
+
+#endif // LE_COMMON_TIMER_H
