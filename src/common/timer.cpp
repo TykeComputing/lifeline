f84130bf75853077da0cc4aeb9c1f5572c698834
Peter Clark <pclark@peterclark.net>
2014-06-11 16:36:19 -0700

    GCC compile error about confliting declaration of size_t.
    - Happens when common/error.h is included from timer.cpp.
    - No idea why I don't define size_t anywhere myself, don't want to waste any more time banging
      my head against this right now.

--- src/common/timer.cpp
+++ src/common/timer.cpp
@@ -21,7 +21,7 @@ along with Lifeline Engine.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "timer.h"
 
-#include <common/error.h>
+//#include <common/error.h>
 
 namespace LE
 {
@@ -42,9 +42,9 @@ float timer::poll(void) const
 {
   Uint64 perf_count_end = SDL_GetPerformanceCounter();
   Uint64 perf_count_elapsed = perf_count_end - p_perf_count_start;
-  
-  LE_ERRORIF(p_perf_freq != SDL_GetPerformanceFrequency(),
-    "Timer error, frequency when polled is different from frequency when reset!");
+
+  //LE_ERRORIF(p_perf_freq != SDL_GetPerformanceFrequency(),
+  //  "Timer error, frequency when polled is different from frequency when reset!");
   return static_cast<float>(perf_count_elapsed) / p_perf_freq;
 }
 
