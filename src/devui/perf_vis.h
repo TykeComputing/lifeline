e2eab71e3f25fec891bb2cf11c08712188f68f13
Peter Clark <pclark@peterclark.net>
2014-07-05 10:45:55 -0700

    Added perf_vis files.

---
+++ src/devui/perf_vis.h
@@ -0,0 +1,23 @@
+/*
+************************************************************************************************
+Copyright 2014 by Peter Clark. All Rights Reserved.
+************************************************************************************************
+*/
+
+#ifndef LE_DEVUI_PERF_VIS_H
+#define LE_DEVUI_PERF_VIS_H
+
+namespace LE
+{
+
+class perf_vis
+{
+public:
+  perf_vis();
+
+private:
+};
+
+} // namespace LE
+
+#endif // LE_DEVUI_PERF_VIS_H
