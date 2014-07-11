928469fea15590132ddf83f18c65427377c05e06
Peter Clark <pclark@peterclark.net>
2014-07-10 17:58:51 -0700

    Moved resource manager from engine to common.

--- src/common/resource_manager.h
+++ src/common/resource_manager.h
@@ -7,15 +7,19 @@ Copyright 2014 by Peter Clark. All Rights Reserved.
 #ifndef LE_COMMON_RESOURCE_MANAGER_H
 #define LE_COMMON_RESOURCE_MANAGER_H
 
+#include <string>
+
 namespace LE
 {
 
 class resource_manager
 {
 public:
-  resource_manager();
+  static void set_resource_dir(std::string const& val);
+  static std::string const& get_resource_dir();
 
 private:
+  static std::string p_resource_dir;
 };
 
 } // namespace LE
