62051c98b0d73be31dc6f6bff8daae07499fed3a
Peter Clark <pclark@peterclark.net>
2014-07-19 18:51:10 -0700

    Documenting and cleaning up perf_vis.

--- src/devui/perf_vis.cpp
+++ src/devui/perf_vis.cpp
@@ -20,17 +20,29 @@ Copyright 2014 by Peter Clark. All Rights Reserved.
 namespace LE
 {
 
+/*!
+* \brief \see logic_component_base
+*/
 unique_id<logic_component_base> const perf_vis::type_id;
 
+/*!
+ * \brief Sets some hard coded graph names/colors (temporary)?
+ *
+ * \todo - Move to devui setup component?
+ */
 perf_vis::perf_vis()
 {
-  // TODO - Move to devui setup component?
   set_graph_color("update", vec4(0.0f, 0.0f, 1.0f, 1.0f));
   set_graph_color("graphics_system", vec4(1.0f, 0.0f, 0.0f, 1.0f));
   set_graph_color("buffer_swap", vec4(1.0f, 1.0f, 0.0f, 1.0f));
   set_graph_color("total_frame", vec4(0.0f, 1.0f, 0.0f, 1.0f));
 }
 
+/*!
+ * \brief Runs performance visualization logic. \see perf_vis for details.
+ *
+ * \param dt - Unused.
+ */
 void perf_vis::update(float dt)
 {
   LE_UNUSED_VAR(dt);
@@ -54,6 +66,10 @@ void perf_vis::update(float dt)
 
   for(auto const& key_record_pair : records)
   {
+    auto const& name = key_record_pair.first;
+    auto const& record = key_record_pair.second;
+    auto const& graph_color = get_graph_color(name);
+
     p_draw_scaffold(
       hud_ddraw,
       curr_bottom_left,
@@ -62,12 +78,17 @@ void perf_vis::update(float dt)
 
     p_draw_graph(
       hud_ddraw,
-      key_record_pair.first,
-      key_record_pair.second,
+      record,
       records.get_max_num_record_entries(),
       curr_bottom_left,
       p_dimensions,
-      get_graph_color(key_record_pair.first));
+      graph_color);
+
+     p_update_graph_label(
+      name,
+      p_get_graph_entry_height_offset(record.front(), p_dimensions.y()),
+      curr_bottom_left,
+      graph_color);
 
     curr_bottom_left += offset_amount;
   }
@@ -86,11 +107,25 @@ void perf_vis::update(float dt)
   p_text_size_is_dirty = false;
 }
 
+/*!
+ * \brief The color of the graph drawn for a \ref profiling_records "profiling_record" entry
+ *   named \p name.
+ *
+ * If a profiling_point named \p name is recorded by this engine's profiling_records, the
+ *   graph drawn for it will have color \p color.
+ *
+ * \note If no color is provided for a graph the color white is used.
+ */
 void perf_vis::set_graph_color(std::string const& name, vec4 const& color)
 {
   p_graph_colors[name] = color;
 }
 
+/*!
+ * \brief Gets the graph color associated with \p name.
+ *
+ * \return The color associated with \p name, if none is specified then color white is returned.
+ */
 vec4 const& perf_vis::get_graph_color(std::string const& name) const
 {
   auto find_it = p_graph_colors.find(name);
@@ -105,6 +140,53 @@ vec4 const& perf_vis::get_graph_color(std::string const& name) const
   }
 }
 
+/*!
+ * \brief The bottom left of the first drawn graph in HUD space.
+ *
+ * \note Default is arbitrary.
+ */
+void perf_vis::set_bottom_left(vec2 const& value)
+{
+  p_bottom_left = value;
+}
+
+/*!
+ * \brief Dimensions of a single graph in HUD space.
+ *
+ * \note Default is arbitrary.
+ */
+void perf_vis::set_dimensions(vec2 const& value)
+{
+  p_dimensions = value;
+}
+
+/*!
+ * \brief How far apart graphs are from eachother, in terms of percent of graph dimension.
+ *
+ * For example: if equal to {0.0, 1.0}, graphics will be displayed directly ontop of eachother
+ *   - as they are offset (1.0 * dimensions.y) from eachother.
+ *
+ * \note Default is zero, all graphs drawn ontop of eachother.
+ *
+ * \remarks
+ * While a negative y offset is possible, a positive offset is preferred as the text
+ *   indicator for \ref set_max_time "the maximum time" will be drawn below the first
+ *   graph.
+ */
+void perf_vis::set_offset_percent(vec2 const& value)
+{
+  p_offset_percent = value;
+}
+
+/*!
+ * \brief Controls how the values on the graph are scaled to fit the \ref set_dimensions
+ *   "dimensions".
+ *
+ * If a value on the graph is equal to max_time it will appear at the dotted line at the top
+ *   of the graph).
+ *
+ * \note Default is approximate frame time for 60 fps (0.016).
+ */
 void perf_vis::set_max_time(float value)
 {
   p_max_time = value;
@@ -115,12 +197,37 @@ void perf_vis::set_max_time(float value)
     true);
 }
 
+/*!
+ * \brief Size of all text used by perf_vis in HUD space.
+ *
+ * \note Default is arbitrary.
+ */
 void perf_vis::set_text_point_size(unsigned value)
 {
   p_text_point_size = value;
   p_text_size_is_dirty = true;
 }
 
+/*!
+ * The horizontal offset at which the name of a graph will appear from the left edge of the
+ *   graph.
+ *
+ * \note Default is 0.0, name drawn right aligned to left side of graph.
+ */
+void perf_vis::set_graph_name_offset(float value)
+{
+  p_graph_name_offset = value;
+}
+
+/*!
+ * \brief Draws scaffolding for a graph which consists of lines along the left and bottom
+ *   of the graph, and a dashed line along the top of the graph.
+ *
+ * \param[out] hud_ddraw - The debug_draw_manager to draw lines with.
+ * \param bottom_left - The bottom left position of the graph.
+ * \param dimensions - The dimensions of the graph.
+ * \param color - The color to use when drawing all lines.
+ */
 void perf_vis::p_draw_scaffold(
     debug_draw_manager & hud_ddraw,
     vec2 const& bottom_left,
@@ -136,17 +243,26 @@ void perf_vis::p_draw_scaffold(
   hud_ddraw.dashed_lines.add_line(top_left, top_right, color * 0.5f);
 }
 
+/*!
+ * \brief Draws a graph from entries in a \ref profiling_records::time_record "time record".
+ *
+ * \param hud_ddraw - The debug_draw_manager to draw lines with.
+ * \param record - The time record to use to draw the graph.
+ * \param max_num_records_entries - Controls the horizontal scaling of the graph. The
+ *   (`max_num_record_entries - 1`)'th entry (see \ref perf_vis for why `- 1` is used will be on
+ *   the Bright hand edge of the graph.
+ * \param bottom_left - The bottom left position of the graph.
+ * \param dimensions - The dimensions of the graph.
+ * \param color - The color to use when drawing all lines.
+ */
 void perf_vis::p_draw_graph(
   debug_draw_manager & hud_ddraw,
-  std::string const& name,
   profiling_records::time_record const& record,
   size_t max_num_records_entries,
   vec2 const& bottom_left,
   vec2 const& dimensions,
-  vec4 const& color)
+  vec4 const& color) const
 {
-  LE_UNUSED_VAR(name);
-
   if(record.empty())
     return;
 
@@ -164,8 +280,9 @@ void perf_vis::p_draw_graph(
       record_entry_it != (record.cend() - 1);
       ++record_entry_it)
   {
-    vec2 curr_point(curr_x, bottom_left.y());
-    curr_point.y() += (*record_entry_it / p_max_time) * dimensions.y();
+    vec2 curr_point(
+      curr_x,
+      bottom_left.y() + p_get_graph_entry_height_offset(*record_entry_it, dimensions.y()) );
 
     hud_ddraw.lines.add_line(prev_point, curr_point, color);
 
@@ -173,28 +290,58 @@ void perf_vis::p_draw_graph(
 
     prev_point = curr_point;
   }
+}
 
-  // Draw graph's name
+/*!
+ * \brief Updates the position of a graph label so that it is aligned to right side of the graph
+ *   horizontally and to the drawn height of the oldest drawn graph vertically.
+ *
+ * \param name - The name of graph, also the text that will be displayed.
+ * \param height_offset - Offset from bottom_left.y() that the text will be vertically centered
+ *   at.
+ * \param bottom_left - The bottom left of the graph.
+ * \param color - Color the text should be displayed in.
+ */
+void perf_vis::p_update_graph_label(
+    std::string const& name,
+    float height_offset,
+    vec2 const& bottom_left,
+    vec4 const& color)
+{
   auto * name_text_ent = p_get_text_entity(name, name, p_text_size_is_dirty);
   auto * name_text_t = name_text_ent->get_component<transform_component>();
   auto * name_text_s = name_text_ent->get_component<sprite_component>();
   name_text_t->set_pos(
     bottom_left.x() - (name_text_s->get_dimensions().x() / 2.0f) - p_graph_name_offset,
-    bottom_left.y() + ((record.front() /p_max_time) * dimensions.y()) );
+    bottom_left.y() + height_offset );
   name_text_s->m_color = color;
 }
 
+/*!
+ * \brief Gets an entity used for displaying text. If one does not exist already one will be
+ *   created.
+ *
+ * \param name - The name associated with this text.
+ * \param text - The text to render
+ * \param update_text_if_preexisting - Forces text to be re-rendered if an entity already
+ *   exists. Otherwise text will only be rendered upon creation of the entity.
+ *
+ * \return A pointer to the entity to be used for \p name.
+ */
 entity * perf_vis::p_get_text_entity(
     std::string const& name,
     std::string const& text,
     bool update_text_if_preexisting)
 {
+  // Prepend arbitrary prefix to the perf_vis owned entities.
   std::string const ent_name = "pv_" + name;
 
+  // Check if we've already created an entity for
   auto * result = get_owning_entity()->find_child(ent_name);
   if(result == nullptr)
   {
-    // An entity for this graph's text does not yet exist, create it.
+    // An entity for this graph's text does not yet exist, create it and set it up for text
+    //   drawing.
     result = get_owning_entity()->get_owning_space()->create_entity(ent_name);
 
     result->create_component<sprite_component>(
@@ -206,6 +353,7 @@ entity * perf_vis::p_get_text_entity(
   {
     if(update_text_if_preexisting)
     {
+      // Re-render text
       auto * text_s = result->get_component<sprite_component>();
       LE_FATAL_ERROR_IF(text_s == nullptr, "Missing sprite component!");
       text_s->set_texture(
@@ -214,6 +362,20 @@ entity * perf_vis::p_get_text_entity(
   }
 
   return result;
+}
+
+/*!
+ * \brief Gets the height offset from the bottom of a graph at which an entry should be
+ *   displayed.
+ *
+ * \param entry_value - The value of the entry.
+ * \param graph_height - The height of the graph.
+ *
+ * \return The height offset.
+ */
+float perf_vis::p_get_graph_entry_height_offset(float entry_value, float graph_height) const
+{
+  return (entry_value / p_max_time) * graph_height;
 }
 
 } // namespace LE
--- src/devui/perf_vis.h
+++ src/devui/perf_vis.h
@@ -29,121 +29,68 @@ class debug_draw_manager;
  *   visualization graphs (using the owning space's HUD debug_draw_manager). Will create new
  *   child \ref entity "entities" to display text.
  *
+ * \note The most recent entry in the profiling_records will be ignored, as it will be assumed
+ *   to be for the frame currently in progress.
  */
 class perf_vis : public logic_component_base
 {
 public:
   perf_vis();
 
-  /*!
-   * \brief Runs performance visualization logic. \see perf_vis for details.
-   *
-   * \param dt - Unused.
-   */
   virtual void update(float dt);
 
-  /*!
-   * \brief The color of the graph drawn for a \ref profiling_records "profiling_record" entry
-   *   named \p name.
-   *
-   * If a profiling_point named \p name is recorded by this engine's profiling_records, the
-   *   graph drawn for it will have color \p color.
-   *
-   * \note If no color is provided for a name the color white is used.
-   */
   void set_graph_color(std::string const& name, vec4 const& color);
   vec4 const& get_graph_color(std::string const& name) const;
 
-  /*!
-   * \brief The bottom left of the first drawn graph in HUD space.
-   *
-   * \note Default is arbitrary.
-   */
-  void set_bottom_left(vec2 const& value) { p_bottom_left = value; }
+  void set_bottom_left(vec2 const& value);
   vec2 const& get_bottom_left() const { return p_bottom_left; }
 
-  /*!
-   * \brief Dimensions of a single graph in HUD space.
-   *
-   * \note Default is arbitrary.
-   */
-  void set_dimensions(vec2 const& value) { p_dimensions = value; }
+  void set_dimensions(vec2 const& value);
   vec2 const& get_dimensions() const { return p_dimensions; }
 
-  /*!
-   * \brief How far apart graphs are from eachother, in terms of percent of graph dimension.
-   *
-   * For example: if {0.0, 1.0}, graphics will be displayed directly ontop of eachother - as
-   *   they are offset (1.0 * dimensions.y) from eachother.
-   *
-   * \note Default is zero, all graphs drawn ontop of eachother.
-   *
-   * \remarks
-   * While a negative y offset is possible, a positive offset is preferred as the text
-   *   indicator for \ref set_max_time "the maximum time" will be drawn below the first
-   *   graph.
-   */
-  void set_offset_percent(vec2 const& value) { p_offset_percent = value; }
+  void set_offset_percent(vec2 const& value);
   vec2 const& get_offset_percent() const { return p_offset_percent; }
 
-  /*!
-   * \brief Controls how the values on the graph are scaled to fit the dimensions (\see
-   *   set_dimensions).
-   *
-   * If a value on the graph is equal to max_time it will appear at the dotted line at the top
-   *   of the graph).
-   *
-   * \note Default is approximate frame time for 60 fps (0.016).
-   */
   void set_max_time(float value);
   float get_max_time() const { return p_max_time; }
 
-  /*!
-   * \brief Size of all text used by this.
-   *
-   * \note Default is arbitrary.
-   */
   void set_text_point_size(unsigned value);
   unsigned get_text_point_size() const { return p_text_point_size; }
 
-  /*!
-   * The horizontal offset at which the name of a graph will appear from the left edge of the
-   *   graph.
-   *
-   * \note Default is 0.0, name drawn right aligned to left side of graph.
-   */
-  void set_graph_name_offset(float value) { p_graph_name_offset = value; }
+  void set_graph_name_offset(float value);
   float get_graph_name_offset() const { return p_graph_name_offset; }
 
-  /*!
-   * \brief \see logic_component_base
-   */
   static unique_id<logic_component_base> const type_id;
 
 private:
   void p_draw_scaffold(
     debug_draw_manager & hud_ddraw,
-    vec2 const& p_bottom_left,
-    vec2 const& p_dimensions,
+    vec2 const& bottom_left,
+    vec2 const& dimensions,
     vec4 const& color) const;
 
   void p_draw_graph(
     debug_draw_manager & hud_ddraw,
-    std::string const& name,
     profiling_records::time_record const& record,
     size_t max_num_records_entries,
-    vec2 const& p_bottom_left,
-    vec2 const& p_dimensions,
-    vec4 const& color); // TODO - const?
+    vec2 const& bottom_left,
+    vec2 const& dimensions,
+    vec4 const& color) const;
+
+  void p_update_graph_label(
+    std::string const& name,
+    float height,
+    vec2 const& bottom_left,
+    vec4 const& color);
 
   entity * p_get_text_entity(
     std::string const& name,
     std::string const& text,
     bool update_text_if_exists);
 
-  std::unordered_map<std::string, vec4> p_graph_colors;
+  float p_get_graph_entry_height_offset(float entry_value, float graph_height) const;
 
-  // Settings
+  std::unordered_map<std::string, vec4> p_graph_colors;
   vec2 p_bottom_left = vec2(-200.f, -100.f);
   vec2 p_dimensions = vec2(400.f, 200.f);
   vec2 p_offset_percent = vec2::zero;
